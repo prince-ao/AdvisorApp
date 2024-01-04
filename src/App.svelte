18b84c1fcb0a8d8ea975b0865d827ff3442d3cbb
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-01-03 23:41:37 -0500

    repo init

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import './app.css';
+  import { onMount } from 'svelte';
+
+  let mouseHold = false;
+	let divElement: HTMLDivElement | null = null;
+
+  onMount(() => {
+    document.addEventListener('mousemove', (e) => {
+      if(mouseHold){
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+
+        if(divElement) {
+          divElement.style.left = `${mouseX - 50}px`;
+          divElement.style.top = `${mouseY - 50}px`;
+        } 
+      }
+    })
+  })
+</script>
+
+<main>
+  <div bind:this={divElement} on:mouseup={() => {mouseHold = false}} on:mousedown={() => {mouseHold = true}} role="button" tabindex="0" class="bg-blue-500 w-[100px] h-[100px] rounded-full absolute cursor-pointer cursor-grab">
+  </div>
+</main>
